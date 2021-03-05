edd2a74cdac79d2784869051332bee8733c56fd2
Robbert Tuerlings <robberttg@protonmail.com>
2021-03-05 22:12:14 +0100

    omn-24
    create overlay
    Update menu options size on mobie

--- src/header/sidebar/index.tsx
+++ src/header/sidebar/index.tsx
@@ -5,21 +5,24 @@ import { FiX } from 'react-icons/fi'
 import logo from '../../images/Logo.png'
 
 const LogoIcon = styled.div`
-    display: flex;
-align-items: center;
+  display: flex;
+  align-items: center;
 `
 
 const CloseIcon = styled.div`
   margin-left: auto;
 `
 const Logo = styled.img`
-    margin: 1rem;
+  margin: 1rem;
 `
 
 const MenuWrapper = styled.nav`
   display: flex;
   justify-content: end;
   align-items: center;
+  @media only screen and (min-width: 416px) {
+      display: none;
+  }
 `
 
 const MobileMenu = styled.aside`
@@ -41,6 +44,17 @@ const MobileMenu = styled.aside`
   box-shadow: 0 12px 24px 0 hsla(0, 0%, 0%, 0.15);
 `
 
+const Overlay = styled.div`
+  background: black;
+  width: 100%;
+  height: ${({ showMenu }) => (showMenu ? '100%' : '0')};;
+  position: fixed;
+  z-index: -1;
+  left: 0;
+  opacity: 0.2;
+  top: 0;
+`
+
 const MenuInnerContainer = styled.div`
   display: grid;
   align-content: center;
@@ -54,18 +68,26 @@ const MenuItem = styled.div`
   margin: 0 2.5rem;
 `
 
+const MenuLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  text-transform: lowercase;
+  width: 80%;
+  display: block;
+`
+
 const Sidebar = ({ showMenu, toggleMenu, items }) => {
   return (
     <MenuWrapper className="menu-wrapper">
-          <MobileMenu showMenu={showMenu} onClick={toggleMenu}>
-              <LogoIcon>
-              <Link to="/">
-                  <Logo src={logo} width={158} />
-              </Link>
-                  <CloseIcon onClick={toggleMenu}>
-          <FiX size={34} style={{ color: 'white', marginRight: '1rem' }} />
-                  </CloseIcon>
-              </LogoIcon>
+      <MobileMenu showMenu={showMenu} onClick={toggleMenu}>
+        <LogoIcon>
+          <Link to="/">
+            <Logo src={logo} width={158} />
+          </Link>
+          <CloseIcon onClick={toggleMenu}>
+            <FiX size={34} style={{ color: 'white', marginRight: '1rem' }} />
+          </CloseIcon>
+        </LogoIcon>
         <MenuInnerContainer>
           {items.map((menuItem, i) => {
             const path = menuItem?.connectedNode?.node?.uri ?? menuItem.url
@@ -80,17 +102,23 @@ const Sidebar = ({ showMenu, toggleMenu, items }) => {
                   'menu-item menu-item-type-custom menu-item-object-custom menu-item-home ' + itemId
                 }
               >
-                <Link
-                  style={{ color: 'white', textDecoration: 'none', textTransform: 'lowercase', width: '80px' }}
+                <MenuLink
+                  // style={{
+                  //   color: 'white',
+                  //   textDecoration: 'none',
+                  //   textTransform: 'lowercase',
+                  //   width: '80px',
+                  // }}
                   to={path}
                   activeClassName={'current-menu-item current_page_item small-letters'}
                 >
                   {menuItem.label}
-                </Link>
+                </MenuLink>
               </MenuItem>
             )
           })}
         </MenuInnerContainer>
+        <Overlay showMenu={showMenu}/>
       </MobileMenu>
     </MenuWrapper>
   )
--- src/templates/front-page/index.tsx
+++ src/templates/front-page/index.tsx
@@ -18,20 +18,20 @@ const MobileWrapper = styled.div`
 `
 
 const HeroContainer = styled.div`
-	@media only screen and (min-width: 416px) {
-		// margin-bottom: 45px;
-		// padding-top: 146px;
-		// width: 685px;
-		// height: 815px;
-		// width: 1750px;
-		margin-bottom: 130px;
-		margin-left: -230px;
-		margin-top: 50px;
-		display: flex;
-		flex-flow: row;
-	}
-	@media only screen and (max-width: 414px) {
-	}
+  @media only screen and (min-width: 416px) {
+    // margin-bottom: 45px;
+    // padding-top: 146px;
+    // width: 685px;
+    // height: 815px;
+    // width: 1750px;
+    margin-bottom: 130px;
+    margin-left: -230px;
+    margin-top: 50px;
+    display: flex;
+    flex-flow: row;
+  }
+  @media only screen and (max-width: 414px) {
+  }
 `
 
 const HeroInnerContainer = styled.div`
@@ -39,98 +39,98 @@ const HeroInnerContainer = styled.div`
 `
 
 const HeroHeader = styled.div`
-  	font-weight: bold;
-	font-stretch: normal;
-	font-style: normal;
-	text-align: left;
-	@media only screen and (min-width: 416px) {
-		margin-bottom: 45px;
-		padding-top: 146px;
-		width: 685px;
-	}
-	@media only screen and (max-width: 414px) {
-		background-color: hsl(241, 64%, 15%);
-		padding: 1rem 2rem;
-		border-bottom-left-radius: 5px;
-		border-bottom-right-radius: 5px;
-		color: var(--color-heading-white);
-	}
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  text-align: left;
+  @media only screen and (min-width: 416px) {
+    margin-bottom: 45px;
+    padding-top: 146px;
+    width: 685px;
+  }
+  @media only screen and (max-width: 414px) {
+    background-color: hsl(241, 64%, 15%);
+    padding: 1rem 2rem;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+    color: var(--color-heading-white);
+  }
 `
 
 const HeroButtonContainer = styled.div`
-	@media only screen and (min-width: 416px) {
-		position: relative;
-		top: 95px;
-		left: 700px;
-		width: fit-content;
-	}
-	@media only screen and (max-width: 414px) {
-		display: none;
-	}
+  @media only screen and (min-width: 416px) {
+    position: relative;
+    top: 95px;
+    left: 700px;
+    width: fit-content;
+  }
+  @media only screen and (max-width: 414px) {
+    display: none;
+  }
 `
 
 const UspServicesContainer = styled.div`
-	background-color: #120c42;
-	border-radius: 5px;
-	@media only screen and (min-width: 416px) {
-		height: 400px;
-		width: 1230px;
-		padding: 80px 0 0 125px;
-	}
-	@media only screen and (max-width: 414px) {
-		padding: 1rem 2rem;
-	}
+  background-color: #120c42;
+  border-radius: 5px;
+  @media only screen and (min-width: 416px) {
+    height: 400px;
+    width: 1230px;
+    padding: 80px 0 0 125px;
+  }
+  @media only screen and (max-width: 414px) {
+    padding: 1rem 2rem;
+  }
 `
 
 const UspServicesInnerContainer = styled.div`
-	border-radius: 5px;
-	flex-flow: row;
-	display: flex;
-	@media only screen and (min-width: 416px) {
+  border-radius: 5px;
+  flex-flow: row;
+  display: flex;
+  @media only screen and (min-width: 416px) {
     padding-top: 80px;
-	}
-	@media only screen and (max-width: 414px) {
+  }
+  @media only screen and (max-width: 414px) {
     background-color: #120c42;
-	}
+  }
 `
 
 const ServicesTextContainer = styled.div`
-	@media only screen and (min-width: 416px) {
-		padding: 37px 55px 0 56px;
-		border-radius: 5px;
-		box-shadow: 0 12px 24px 0 hsla(0, 0%, 0%, 0.15);
-		background-color: hsl(264, 71%, 43%);
-	}
-	@media only screen and (max-width: 414px) {
-	}
+  @media only screen and (min-width: 416px) {
+    padding: 37px 55px 0 56px;
+    border-radius: 5px;
+    box-shadow: 0 12px 24px 0 hsla(0, 0%, 0%, 0.15);
+    background-color: hsl(264, 71%, 43%);
+  }
+  @media only screen and (max-width: 414px) {
+  }
 `
 
 const ServicesContent = styled.div`
-	font-size: 16px;
-	font-weight: 300;
-	font-stretch: normal;
-	font-style: normal;
-	line-height: 1.88;
-	letter-spacing: 0.24px;
-	text-align: left;
-	color: hsl(0, 0%, 100%);
-	display: flex;
-	flex-flow: column;
+  font-size: 16px;
+  font-weight: 300;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.88;
+  letter-spacing: 0.24px;
+  text-align: left;
+  color: hsl(0, 0%, 100%);
+  display: flex;
+  flex-flow: column;
 `
 
 const ServicesButton = styled.div`
   position: relative;
 
-	@media only screen and (min-width: 416px) {
-		top: 26px;
-		left: 230px;
-		max-width: 138.55px;
-	}
-	@media only screen and (max-width: 414px) {
-		max-width: 94.73px;
-		top: 32px;
-		left: -60px;
-	}
+  @media only screen and (min-width: 416px) {
+    top: 26px;
+    left: 230px;
+    max-width: 138.55px;
+  }
+  @media only screen and (max-width: 414px) {
+    max-width: 94.73px;
+    top: 32px;
+    left: -60px;
+  }
 `
 
 const BlogWrapper = styled.div`
@@ -141,7 +141,6 @@ const BlogWrapper = styled.div`
     place-content: center;
   }
 `
-
 
 const Homepage = (props) => {
   const {
@@ -156,7 +155,6 @@ const Homepage = (props) => {
         <>
           <div id="primary" className="content-area">
             <main id="main" className="site-main">
-
               <MobileWrapper>
                 <HeroContainer>
                   <HeroHeader>
@@ -169,20 +167,20 @@ const Homepage = (props) => {
               </MobileWrapper>
 
               <DesktopWrapper>
-              <HeroContainer>
-                <HeroInnerContainer>
-                  <HeroHeader>
-                    <p className="hero-small">{parse(homepageACF.heroSubHeader)}</p>
-                    <h1 className="hero-big">{parse(homepageACF.heroHeader)}</h1>
-                  </HeroHeader>
-                  <p className="hero-subtext">{parse(homepageACF.heroSubtext)}</p>
-                  <HeroButtonContainer>
-                    <div className="hero-button-square">
-                      <div className="hero-button-triangle"></div>
-                    </div>
-                  </HeroButtonContainer>
-                </HeroInnerContainer>
-                <img className="hero-image" src={homepageACF.heroImage.sourceUrl} alt="" />
+                <HeroContainer>
+                  <HeroInnerContainer>
+                    <HeroHeader>
+                      <p className="hero-small">{parse(homepageACF.heroSubHeader)}</p>
+                      <h1 className="hero-big">{parse(homepageACF.heroHeader)}</h1>
+                    </HeroHeader>
+                    <p className="hero-subtext">{parse(homepageACF.heroSubtext)}</p>
+                    <HeroButtonContainer>
+                      <div className="hero-button-square">
+                        <div className="hero-button-triangle"></div>
+                      </div>
+                    </HeroButtonContainer>
+                  </HeroInnerContainer>
+                  <img className="hero-image" src={homepageACF.heroImage.sourceUrl} alt="" />
                 </HeroContainer>
               </DesktopWrapper>
 
