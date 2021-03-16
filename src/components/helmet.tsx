1fbac732faf057e12eb59e19d8501a85347ce1b0
Robbert Tuerlings <robberttg@protonmail.com>
2021-03-16 21:34:11 +0100

    Update helmet.tsx

--- src/components/helmet.tsx
+++ src/components/helmet.tsx
@@ -5,6 +5,7 @@ const AutoHelmet = ({ title }) => {
   return (
     <Helmet>
       <title>{title} - Omnia Consultancy</title>
+      <meta name="description" content="Informatie, communicate & technologie. Optimaal, begrijpelijk en menselijk toegepast." />
     </Helmet>
   )
 }
